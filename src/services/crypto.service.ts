5416851c097518adcdee22070d54116af69abd90
regionstats <joshpearsonrs@gmail.com>
2018-09-16 00:12:49 -0500

    fixed crypto service

--- src/services/crypto.service.ts
+++ src/services/crypto.service.ts
@@ -1,12 +1,7 @@
 import * as crypto from "crypto";
 class CryptoService {
-    _hash: crypto.Hash;
-    constructor(){
-        this._hash = crypto.createHash("sha256");
-    }
-
     getHash(str): string {
-        return this._hash.update(str).digest('base64');
+        return crypto.createHash("sha256").update(str).digest('base64');
     }
 }
 
