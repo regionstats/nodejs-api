c8bbb555b98c301463874cc7118a949e011b818f
regionstats <joshpearsonrs@gmail.com>
2018-09-18 23:23:27 -0500

    api now allows cors

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,19 @@ class App {
     constructor() {
         this.app = express();
         this.app.use(bodyParser.json()); 
+        this.app.use(this.allowCors);
         this.app.route('/stat').post(AddStat);
 
         this.app.listen(config.port, () => {
             console.log('server listening on port ' + config.port);
         })
     }
+
+    allowCors(req: express.Request, res:express.Response, next: express.NextFunction) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    } 
 }
 
 export default new App().app;
--- src/route-handlers/stat.ts
+++ src/route-handlers/stat.ts
@@ -4,16 +4,14 @@ import  IpfsService  from '../services/ipfs.service';
 import  CryptoService  from '../services/crypto.service';
 import { validateStat } from "@regionstats/validator";
 import { Stat } from "@regionstats/models";
-import { AddStatRequest } from "../models/add-stat-request";
+import { AddStatRequest } from "@regionstats/models";
 import { StatEntity } from "../models/stat-entity";
 
 export async function AddStat(req: Request, res: Response) { 
     let request = <AddStatRequest>req.body  ;        
-    let errorMessage = validateStat(request.stat) || AddStatRequest.validate(request);
+    let errorMessage = validateRequest(request);
     if (errorMessage){
-        res.status(400).send({
-            "error": errorMessage
-        });
+        res.status(400).send(errorMessage);
         return;
     } 
     Stat.clean(request.stat);
@@ -28,4 +26,41 @@ export async function AddStat(req: Request, res: Response) {
     StatEntity.clean(statEntity);
     DatabaseService.addStat(statEntity);
     res.status(200).send(statEntity._hash);
+}
+
+async function validateRequest(request: AddStatRequest){
+    if (request.hasOwnProperty("name")){
+        if (typeof request.name != "string"){
+            return "name must be a string";
+        }
+        if (request.name.length > 50){
+            return "name must be under 50 characters";
+        }
+    }
+    if (request.hasOwnProperty("tripcodeKey")){
+        if (typeof request.tripcodeKey != "string"){
+            return "tripcodeKey must be a string";
+        }
+        if (request.tripcodeKey.length > 256){
+            return "tripcodeKey must be under 256 characters";
+        }
+    }
+    if (request.hasOwnProperty("category")){
+        if (typeof request.category != "string"){
+            return "category must be a string";
+        }
+        if (request.category.length > 30){
+            return "tripcodeKey must be under 30 characters";
+        }
+    }
+    if (!request.hasOwnProperty("stat")){
+        return "missing stat property";
+    }
+    if (typeof request.stat != "object"){
+        return "stat must be an object";
+    }
+    let result = await validateStat(request.stat);
+    if (result){
+        return "error with stat: " + result;
+    }
 }
